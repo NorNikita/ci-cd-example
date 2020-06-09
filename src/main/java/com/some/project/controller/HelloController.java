45d97df00a243be7b274725f9a8144dcb64dd55d
Nor Nikita <nor.nikit@yandex.ru>
2020-06-09 14:23:51 +0300

    add controller, port, dockerfile for application

---
+++ src/main/java/com/some/project/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.some.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String hello(@PathVariable String name) {
+        return "Hello" + name;
+    }
+}
