8373e7396e1ecaeffb31d3776c815c6d4014e820
Nor Nikita <nor.nikit@yandex.ru>
2020-06-13 00:46:03 +0300

    update controller

--- ci-cd/deploy/prepare.sh
+++ ci-cd/deploy/prepare.sh
@@ -5,6 +5,7 @@ cd ~/application/target
 ls -l
 filename=$( find *.jar )
 
+pwd
 echo "rm -rf /app/*" | ssh -p 49180 root@ovz5.9324240515.pqj7n.vps.myjino.ru /bin/bash
 scp -P 49180 ${filename} root@ovz5.9324240515.pqj7n.vps.myjino.ru:~/app
 scp -P 49180 Dockerfile root@ovz5.9324240515.pqj7n.vps.myjino.ru:~/app
