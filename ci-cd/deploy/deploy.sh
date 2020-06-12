1fdd9c7727629b75679d2fa12528272f1e2d86e5
Nor Nikita <nor.nikit@yandex.ru>
2020-06-13 01:28:04 +0300

    update deploy.sh

--- ci-cd/deploy/deploy.sh
+++ ci-cd/deploy/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cat ./prepare.sh | ssh -p 49180 root@ovz5.9324240515.pqj7n.vps.myjino.ru /bin/bash
+
+cd ~/workspace/build_deploy/ci-cd/deploy
 scp -P 49180 Dockerfile root@ovz5.9324240515.pqj7n.vps.myjino.ru:~/app
 
 cd ~/application/target
